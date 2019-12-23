9ff93a8f8252affa86c81508f1d4bbd2e038ec42
ecpshin <francisco.shin@gmail.com>
2019-12-23 19:58:32 -0300

    Exercicio3 finalizado

--- src/application2/Exercise3.java
+++ src/application2/Exercise3.java
@@ -10,6 +10,23 @@ public class Exercise3 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Ler dois número inteiros e verificar de eles são múltiplos");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a > b) {
+			if(a % b == 0) {
+				System.out.println("Sao Multiplos.");
+			} else {
+				System.out.println("Nao sao multiplos.");
+			}
+		} else if (b > a) {
+			if(b % a == 0) {
+				System.out.println("Sao multiplos");
+			} else {
+				System.out.println("Nao sao multiplos.");
+			}
+		}
 		sc.close();
 
 	}
